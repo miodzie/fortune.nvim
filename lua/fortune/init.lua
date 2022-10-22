c07555cb39baafee84e3aa8cf861e3046f9848cc
miodzie <miodzie@protonmail.com>
2022-10-21 21:33:28 -0400

    Finish a MVP kinda.

--- lua/fortune/init.lua
+++ lua/fortune/init.lua
@@ -1,1 +1,7 @@
-print 'hello, world!'
+local scripts = vim.api.nvim_exec("!fortune -s", true)
+local output = vim.split(scripts, '\n')
+for k,v in pairs(output) do
+        if k > 1 and v ~= '' then
+                print(vim.trim(v))
+        end
+end
---
+++ plugin/fortune.lua
@@ -0,0 +1,1 @@
+-- print "plugin dir load fortuned"
